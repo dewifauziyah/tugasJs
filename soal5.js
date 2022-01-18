6504fcba7df4278e9a59b334a875ab764df443f8
dewifauziyah <dewifauziyah549@gmail.com>
2022-01-18 13:46:44 +0700

    tugasjs

---
+++ soal5.js
@@ -0,0 +1,20 @@
+var perkalian=0
+var pembagian=0
+var penjumlahan=0
+var pengurangan=0
+
+function calculator (a,b) { 
+    perkallian=a*b
+    pembagian=a/b
+    penjumlahan=a+b
+    pengurangan=a-b
+}
+
+calculator (3,2)
+console.log (perkallian)
+calculator (10,2)
+console.log (pembagian)
+calculator (76,12)
+console.log (penjumlahan)
+calculator (789,10)
+console.log (pengurangan)
